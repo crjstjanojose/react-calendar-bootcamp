c4b92d10df5cfb6269877b681f9fb194ce19ecd2
Cristiano Silva <crjstjanojose@gmail.com>
2021-06-24 14:04:53 -0300

    Tela de Loing Front

--- src/app/LoginScreen.tsx
+++ src/app/LoginScreen.tsx
@@ -1,3 +1,49 @@
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import { useState } from "react";
+
 export function LoginScreen() {
-  return <div>Tela de Login</div>;
+  const [email, setEmail] = useState("danilio@email.com");
+  const [password, setPassword] = useState("1234");
+
+  function signIn(evt: React.FormEvent) {
+    evt.preventDefault();
+    console.log("signIn");
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <h1>Agenda React</h1>
+      <p>
+        Digite e-mail e senha para entrar no sistema, Para testar, use o e-mail{" "}
+        <kbd>danilo@email.com</kbd>e a senha <kbd>1234</kbd>.
+      </p>
+      <form onSubmit={signIn}>
+        <TextField
+          label="E-mail"
+          variant="outlined"
+          fullWidth
+          type="text"
+          margin="normal"
+          value={email}
+          onChange={(evt) => setEmail(evt.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          fullWidth
+          type="password "
+          value={password}
+          onChange={(evt) => setPassword(evt.target.value)}
+        />
+        <Box marginTop="10px" textAlign="right">
+          <Button variant="contained" color="primary" type="submit">
+            Entrar
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  );
 }
